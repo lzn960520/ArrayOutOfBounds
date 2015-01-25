c0408b0a8994f0886753963e849ec4ea8021ec7b
rododo <lzn960520@126.com>
2015-01-25 17:26:27 +0800

    fix some typo

--- modules/user.js
+++ modules/user.js
@@ -1,15 +1,20 @@
 module.exports = function(env) {
-  function checkUsername(name){
+  function checkUsername(name) {
     return name.length >= 6;
   }
-  function checkPassword(password){
+  function checkPassword(password) {
     return password.length >= 6;
   }
   function handleLogin(req, res) {
-    req.db.collection('users', { safe: true, strict: true }, function(err, collection) {
+    req.db.collection('users', {
+      safe : true,
+      strict : true
+    }, function(err, collection) {
       if (err)
         throw new Error(err);
-      collection.find({ username: req.data.username }).toArray(function(err, docs) {
+      collection.find({
+        username : req.data.username
+      }).toArray(function(err, docs) {
         if (docs.length == 0)
           res.fail("User does not exist");
         else if (docs[0].password != req.data.password)
@@ -18,59 +23,77 @@ module.exports = function(env) {
           res.session.role = docs[0].role;
           res.session.username = docs[0].username;
           res.success({
-            "username": docs[0].username,
-            "role": docs[0].role
+            "username" : docs[0].username,
+            "role" : docs[0].role
           });
         }
       });
     });
   }
-  
+
   function handleRegister(req, res) {
     var username = req.data.username;
     var password = req.data.password;
-    req.db.collection('users', { safe: true, strict: true }, function(err, collection) {
-      if (err)
-        throw new Error(err);
-      if (!check_username(username))
-        res.fail("Username should be a string whose length is larger than 6 and consist of number and alphabets"); 
-      else if (!check_password(password))
-        res.fail("Password should be a string whose length is larger than 6 and consist of number and alphabets"); 
-      else
-        collection.count({ "username": username }, function(err, count) {
-          if (err)
-            throw new Error(err);
-          if (count != 0)
-            res.fail("Username has been taken");
-          else {
-            collection.insert({
-              "username": username,
-              "password": password,
-              "role": "user"
-            }, {safe:true}, function(err, result) {
+    req.db
+        .collection(
+            'users',
+            {
+              safe : true,
+              strict : true
+            },
+            function(err, collection) {
               if (err)
                 throw new Error(err);
-              res.success({
-                "username": username,
-                "role": "user"
-              });
+              if (!checkUsername(username))
+                res
+                    .fail("Username should be a string whose length is larger than 6 and consist of number and alphabets");
+              else if (!checkPassword(password))
+                res
+                    .fail("Password should be a string whose length is larger than 6 and consist of number and alphabets");
+              else
+                collection.count({
+                  "username" : username
+                }, function(err, count) {
+                  if (err)
+                    throw new Error(err);
+                  if (count != 0)
+                    res.fail("Username has been taken");
+                  else {
+                    collection.insert({
+                      "username" : username,
+                      "password" : password,
+                      "role" : "user"
+                    }, {
+                      safe : true
+                    }, function(err, result) {
+                      if (err)
+                        throw new Error(err);
+                      res.session.role = "user";
+                      res.session.username = username;
+                      res.success({
+                        "username" : username,
+                        "role" : "user"
+                      });
+                    });
+                  }
+                });
             });
-          }
-        }); 
-    });
   }
-  
+
   function handleLogout(req, res) {
     res.session.username = "";
     res.session.role = "nologin";
     res.success({
-      "username": "",
-      "role": "nologin"
+      "username" : "",
+      "role" : "nologin"
     });
   }
-  
+
   env.registerHandler("login", handleLogin);
   env.registerHandler("register", handleRegister);
   env.registerHandler("logout", handleLogout);
-  env.registerDefaultSession({ "username": "", "role": "nologin" });
+  env.registerDefaultSession({
+    "username" : "",
+    "role" : "nologin"
+  });
 }
--- upload_server.js
+++ upload_server.js
@@ -3,17 +3,21 @@ var bodyParser = require('body-parser');
 
 // Run upload server
 var webapp = express();
-webapp.use(bodyParser.urlencoded({extended:false}));
+webapp.use(bodyParser.urlencoded({
+  extended : false
+}));
 webapp.use(require('connect-multiparty')());
 webapp.post("/uploadcasefile", function(req, res) {
   var session = req.body.session;
-  if ((typeof session == "undefined") || (session == null) || (session == "null") || (session == "")) {
+  if ((typeof session == "undefined") || (session == null) ||
+      (session == "null") || (session == "")) {
     session = env.randomMD5();
     fs.mkdirSync(__dirname + "/tmp/" + session + "/");
   }
   var files = req.files.upload;
   if (!isArray(files))
-    files = [files];
+    files = [ files ];
+  console.log("Upload handle " + files);
   var processed = 0;
   files.forEach(function(file) {
     if (file.name.match(/[1-9][0-9]*.(in|out)/)) {
@@ -24,36 +28,39 @@ webapp.post("/uploadcasefile", function(req, res) {
           return;
         }
         file.name = file.name.match(/([1-9][0-9]*.(in|out))/)[0];
-        fs.writeFile(__dirname + "/tmp/" + session + "/" + file.name, data, function(err) {
-          if (err) {
-            console.log(err);
-            processed++;
-            return;
-          }
-          processed++;
-          if (processed == files.length)
-            find_missing_casefile(session, Number(req.body.max_case), function(missing) {
-              res.end(
-                JSON.stringify({
-                  "missing": missing,
-                  "session": session
-                })
-              );
+        fs.writeFile(
+            __dirname + "/tmp/" + session + "/" + file.name,
+            data,
+            function(err) {
+              if (err) {
+                console.log(err);
+                processed++;
+                return;
+              }
+              processed++;
+              if (processed == files.length)
+                find_missing_casefile(
+                    session,
+                    Number(req.body.max_case),
+                    function(missing) {
+                      res.end(JSON.stringify({
+                        "missing" : missing,
+                        "session" : session
+                      }));
+                    });
             });
-        });
         fs.unlink(file.path);
       })
     } else {
       fs.unlink(file.path);
       processed++;
       if (processed == files.length)
-        find_missing_casefile(session, Number(req.body.max_case), function(missing) {
-          res.end(
-            JSON.stringify({
-              "missing": missing, 
-              "session": session
-            })
-          );
+        find_missing_casefile(session, Number(req.body.max_case), function(
+            missing) {
+          res.end(JSON.stringify({
+            "missing" : missing,
+            "session" : session
+          }));
         });
     }
   });
