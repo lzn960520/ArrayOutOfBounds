e098f104f65617aa7f7146d5f743c70ddc249cb3
rododo <lzn960520@126.com>
2015-02-23 01:00:34 +0800

    change something about jshint

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular
+var app = angular // jshint ignore:line
     .module("outofbounds", [ "ui.bootstrap", "ngRoute", "ui.ace" ]);
 app.config([ "$routeProvider", function(routeProvider) {
   routeProvider.otherwise({
